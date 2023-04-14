5be22265211b8747dfcc504d0e68189cb07fe5a5
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-14 15:20:32 +0200

    Second step

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -16,3 +16,11 @@ for (var i = 0; i < favoriteFoods.Length; i++)
 {
     Console.WriteLine($"{i + 1}. {favoriteFoods[i]}");
 }
+
+Console.WriteLine("");
+
+// Stampa prima ed ultima posizione della classifica:
+
+Console.WriteLine($"Il cibo che preferisco di più è: {favoriteFoods[0]}");
+Console.WriteLine($"il cibo che preferisco di meno è: {favoriteFoods[6]}");
+
