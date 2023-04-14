00c8b3c1a0e910239b6be74f227e19e0ddfdde21
Ahurricaneeos <aurora_sala@hotmail.it>
2023-04-14 15:05:34 +0200

    First step

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Array cibi preferiti:
+
+string[] favoriteFoods = {"Pizza", "Hamburger", "Patatine", "Sushi", "Panna cotta", "Sacher", "Cioccolato"};
+
+// Stampa lunghezza array:
+
+Console.WriteLine("Lunghezza array: " + favoriteFoods.Length);
+Console.WriteLine("");
+
+// Stampa elementi array in ordine di preferenza:
+
+Console.WriteLine("Cibi preferiti in ordine dal più al meno preferito: ");
+Console.WriteLine("");
+
+for (var i = 0; i < favoriteFoods.Length; i++)
+{
+    Console.WriteLine($"{i + 1}. {favoriteFoods[i]}");
+}
